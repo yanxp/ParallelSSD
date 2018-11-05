70548b663e3325561d6d8588ce2afafb6a1f9e8e
yanxiaopeng <yanxiaopeng@sensetime.com>
2018-11-05 16:37:20 +0800

    demo

--- srun_demo_logo.sh
+++ srun_demo_logo.sh
@@ -1,1 +1,1 @@
-srun --partition=Bigvideo --mpi=pmi2 --gres=gpu:1 --job-name=Yolo --kill-on-bad-exit=1 python demo_logo.py -d Logo -v RFB_E_vgg -s 512 --trained_model logo149RFB_E_vgg_Logo_epoches_65.pth --demo data/Logo/testJPEG/burgerkingimg000377.jpg
+srun --partition=SenseMediaF --mpi=pmi2 --gres=gpu:1 --job-name=Yolo --kill-on-bad-exit=1 python demo_logo.py -d Logo -v RFB_E_vgg -s 512 --trained_model logo19RFB_E_vgg_Logo_epoches_130.pth
